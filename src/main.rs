1f5e06f24d3db27b1ee9c5bdce01dc4ce447e735
Maxim B <b.maxim.dev@gmail.com>
2023-05-11 16:02:20 +0400

    empty

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::{
 };
 
 fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT").unwrap_or_else(|_| "34254".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "80".to_string());
 
-    let listener = TcpListener::bind(String::from_iter(["0.0.0.0:", &port])).unwrap();
+    let listener = TcpListener::bind(String::from_iter(["127.0.0.1:", &port])).unwrap();
 
     for stream in listener.incoming() {
         handle_connection(stream.unwrap());
@@ -20,9 +20,9 @@ fn handle_connection(mut stream: TcpStream) {
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        "GET /sleep HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        "GET / HTTPS/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTPS/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        _ => ("HTTPS/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -31,4 +31,13 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+mod tests {
+    #[test]
+    fn correct_address() {
+        let port = std::env::var("PORT").unwrap_or_else(|_| "34254".to_string());
+
+        assert_eq!(String::from_iter(["0.0.0.0:", &port]), "0.0.0.0:34254");
+    }
 }
